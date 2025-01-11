24f430df7b06b1d276315e63443ba5584b588a46
johnvywilson <139486606+johnvywilson@users.noreply.github.com>
2025-01-10 19:11:44 -0500

    first commit

---
+++ ice/src/client/app/animals/animals.service.js
@@ -0,0 +1,7 @@
+/*
+    Name: Johnvy Wilson
+    filename: app.js
+    Course: INFT 2202
+    Date: Januiary 10, 2025
+    Description: This is my general animal service script.
+*/ 
---
+++ ice/src/client/app/app.js
@@ -0,0 +1,7 @@
+/*
+    Name: Johnvy Wilson
+    filename: app.js
+    Course: INFT 2202
+    Date: January 10, 2025
+    Description: This is my general app script.
+*/
