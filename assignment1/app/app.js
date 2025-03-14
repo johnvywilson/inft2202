e525cee0fbfb89e7d7330d4e8abeaac93b86e00b
johnvywilson <139486606+johnvywilson@users.noreply.github.com>
2025-03-13 22:37:33 -0400

    Assignment1
    done

---
+++ assignment1/app/app.js
@@ -0,0 +1,6 @@
+// Name: Johnvy Wilson
+// Course: INFT 2202
+// Date: 2025-03-13
+// Description:A page for my shop.
+// Set the current year in the footer
+document.getElementById('copyrightYear').textContent = new Date().getFullYear();
